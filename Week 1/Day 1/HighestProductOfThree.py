50fd4edfb51551e60bd50909262b00fd384c8768
viseshthakur <31198554+viseshthakur@users.noreply.github.com>
2018-06-26 15:30:59 +0530

    Create HighestProductOfThree.py

---
+++ Week 1/Day 1/HighestProductOfThree.py
@@ -0,0 +1,63 @@
+import unittest
+
+
+def highest_product_of_3(list_of_ints):
+
+    # Calculate the highest product of three numbers
+    
+    leng= len(list_of_ints)
+    if leng<3:
+        raise Exception("Exception Raised")
+    sortlist = sorted(list_of_ints)
+
+    a = sortlist[leng -1] * sortlist[leng -2] * sortlist[leng -3]
+    b = sortlist[leng -1] * sortlist[0] * sortlist[1]
+
+    if a > b:
+        return a
+    else:
+        return b
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_list(self):
+        actual = highest_product_of_3([1, 2, 3, 4])
+        expected = 24
+        self.assertEqual(actual, expected)
+
+    def test_longer_list(self):
+        actual = highest_product_of_3([6, 1, 3, 5, 7, 8, 2])
+        expected = 336
+        self.assertEqual(actual, expected)
+
+    def test_list_has_one_negative(self):
+        actual = highest_product_of_3([-5, 4, 8, 2, 3])
+        expected = 96
+        self.assertEqual(actual, expected)
+
+    def test_list_has_two_negatives(self):
+        actual = highest_product_of_3([-10, 1, 3, 2, -10])
+        expected = 300
+        self.assertEqual(actual, expected)
+
+    def test_list_is_all_negatives(self):
+        actual = highest_product_of_3([-5, -1, -3, -2])
+        expected = -6
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_list(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([])
+
+    def test_error_with_one_number(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1])
+
+    def test_error_with_two_numbers(self):
+        with self.assertRaises(Exception):
+            highest_product_of_3([1, 1])
+
+
+unittest.main(verbosity=2)
